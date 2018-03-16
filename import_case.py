f23d704dd71e6bc5dac88f4f6a7a895a3397d6f0
yeahbeen <yeahbeen@163.com>
2018-03-16 14:24:25 +0800

    修改

--- import_case.py
+++ import_case.py
@@ -2,9 +2,9 @@
 作用：
     把excel的用例转化成能导入到testlink的xml
 使用方法：
-    1、把用例Excel文件（xlsx格式，每次只放一个）和该脚本放到同一个目录
+    1、把用例Excel文件和该脚本放到同一个目录
     2、命令行进入该目录，执行python import_case.py
-    3、执行结束后，就会生成xml，原来Excel的每个sheet对应一个xml，这些xml可以直接导入到testlink
+    3、执行结束后，就会生成能导入到testlink的xml，每个Excel对应一个文件夹，Excel的每个sheet对应一个xml。
 使用条件：
     1、安装Python3，安装openpyxl包
     2、Excel要符合一定的格式：
@@ -20,164 +20,167 @@ from openpyxl.worksheet.cell_range import CellRange
 
 dir = __file__+"\\..\\"
 
-filename = None
+filename = []
 for file in os.listdir(dir):
-    print(file)
+    # print(file)
     # print(os.path.splitext(file)[1])
     if os.path.splitext(file)[1] == ".xlsx": 
-        filename = file
-        break
+        filename.append(file)
         
-if filename == None:
+if filename == []:
     print("没有Excel文件")
     sys.exit()
     
-result_dir = dir + filename.replace(".xlsx","") + "\\"
-os.makedirs(result_dir)
     
-wb = load_workbook(filename)
-
-print(wb.sheetnames)
-for sheetname in wb.sheetnames:
-    # print(sheetname)
-    if sheetname == "Sheet1": continue
-
-    sheet_ranges = wb[sheetname]
-
-    
-    def ismerged(cell):
-        for r in sheet_ranges.merged_cells.ranges:
-            if r.issuperset(CellRange(cell.coordinate)): return True
-        return False
         
-    def getmerge(cell):
-        for r in sheet_ranges.merged_cells.ranges:
-            if r.issuperset(CellRange(cell.coordinate)): return r
-        return None
+def ismerged(cell):
+    for r in sheet_ranges.merged_cells.ranges:
+        if r.issuperset(CellRange(cell.coordinate)): return True
+    return False
+    
+def getmerge(cell):
+    for r in sheet_ranges.merged_cells.ranges:
+        if r.issuperset(CellRange(cell.coordinate)): return r
+    return None
 
 
-    SUITE=r"""<testsuite name="%(suitename)s" >
-    <node_order><![CDATA[1]]></node_order>
-    <details><![CDATA[]]></details> 
-    %(testcases)s
-    </testsuite>
-    """
-    HEADER = r"""<?xml version="1.0" encoding="UTF-8"?>
-    """
+SUITE=r"""<testsuite name="%(suitename)s" >
+<node_order><![CDATA[1]]></node_order>
+<details><![CDATA[]]></details> 
+%(testcases)s
+</testsuite>
+"""
+HEADER = r"""<?xml version="1.0" encoding="UTF-8"?>
+"""
 
-    TESTCASE = r"""<testcase internalid="198575" name="%(casename)s">
-        <summary><![CDATA[<p>
-        %(casename)s</p>
-    ]]></summary>
-        <preconditions><![CDATA[<p>
-        %(preconditions)s</p>
-    ]]></preconditions>
-        <execution_type><![CDATA[1]]></execution_type>
-        <importance><![CDATA[2]]></importance>
-    <steps>
-    <step>
-        <step_number><![CDATA[1]]></step_number>
-        <actions><![CDATA[<p>
-        %(steps)s</p>
-    ]]></actions>
-        <expectedresults><![CDATA[<p>
-        %(expect)s</p>
-    ]]></expectedresults>
-        <execution_type><![CDATA[1]]></execution_type>
-    </step>
-    </steps>
-    </testcase>
-    """
+TESTCASE = r"""<testcase internalid="198575" name="%(casename)s">
+    <summary><![CDATA[<p>
+    %(casename)s</p>
+]]></summary>
+    <preconditions><![CDATA[<p>
+    %(preconditions)s</p>
+]]></preconditions>
+    <execution_type><![CDATA[1]]></execution_type>
+    <importance><![CDATA[2]]></importance>
+<steps>
+<step>
+    <step_number><![CDATA[1]]></step_number>
+    <actions><![CDATA[<p>
+    %(steps)s</p>
+]]></actions>
+    <expectedresults><![CDATA[<p>
+    %(expect)s</p>
+]]></expectedresults>
+    <execution_type><![CDATA[1]]></execution_type>
+</step>
+</steps>
+</testcase>
+"""
+    
+    
+for file in filename:
+    print("\n"+file)
+    result_dir = dir + file.replace(".xlsx","") + "\\"
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+        
+    wb = load_workbook(file)
 
-    suites = []
+    print(wb.sheetnames)
+    for sheetname in wb.sheetnames:
+        print(sheetname)
+        if sheetname == "Sheet1": continue
 
+        sheet_ranges = wb[sheetname]
 
-    i=11
-    casename0=None
-    preconditions0=""
-    m0 = None
-    suitename0 = ""
-    suite = None
-    j = 2
-    cases = []
-    while sheet_ranges['B'+str(i)].value:
-        #确定模块
-        if sheet_ranges['C'+str(i)].value:
-            if suitename0 != "":
-                print(suitename0+":"+str(len(cases)))
-                suite = SUITE % dict(suitename = suitename0,testcases = "".join(cases))
-                cases = []
-                suites.append(suite)
-            m0 = getmerge(sheet_ranges['C'+str(i)])
-            suitename0 = sheet_ranges['C'+str(i)].value
-            suitename0 = suitename0.replace("\"","“").replace("&","_")
-            # suite = SUITE% dict(suitename = suitename0)
-        elif suitename0 == "" or not ismerged(sheet_ranges['C'+str(i)]) or getmerge(sheet_ranges['C'+str(i)]) != m0:
-            if suitename0 != "":
-                print(suitename0+":"+str(len(cases)))
-                suite = SUITE % dict(suitename = suitename0,testcases = "".join(cases))
-                cases = []
-                suites.append(suite)
-            m0 = getmerge(sheet_ranges['C'+str(i)])
-            suitename0 = "未知模块"
-            # suite = SUITE% dict(suitename = suitename0)
+        suites = []
+
+        i=11
+        casename0=None
+        preconditions0=""
+        m0 = None
+        suitename0 = ""
+        suite = None
+        j = 2
+        cases = []
+        while sheet_ranges['B'+str(i)].value:
+            #确定模块
+            if sheet_ranges['C'+str(i)].value:
+                if suitename0 != "":
+                    print(suitename0+":"+str(len(cases)))
+                    suite = SUITE % dict(suitename = suitename0,testcases = "".join(cases))
+                    cases = []
+                    suites.append(suite)
+                m0 = getmerge(sheet_ranges['C'+str(i)])
+                suitename0 = sheet_ranges['C'+str(i)].value
+                suitename0 = suitename0.replace("\"","“").replace("&","_")
+                # suite = SUITE% dict(suitename = suitename0)
+            elif suitename0 == "" or not ismerged(sheet_ranges['C'+str(i)]) or getmerge(sheet_ranges['C'+str(i)]) != m0:
+                if suitename0 != "":
+                    print(suitename0+":"+str(len(cases)))
+                    suite = SUITE % dict(suitename = suitename0,testcases = "".join(cases))
+                    cases = []
+                    suites.append(suite)
+                m0 = getmerge(sheet_ranges['C'+str(i)])
+                suitename0 = "未知模块"
+                # suite = SUITE% dict(suitename = suitename0)
+                
+            #确定用例名
+            if sheet_ranges['D'+str(i)].value:
+                casename = sheet_ranges['D'+str(i)].value
+                casename = casename.replace("&","_").replace("\"","“")
+                casename0=casename
+                j = 2
+            elif casename0:
+                casename = "%s(%i)"%(casename0,j)
+                j+=1
+            elif sheet_ranges['E'+str(i)].value:
+                casename = sheet_ranges['E'+str(i)].value
+            else:
+                casename = sheet_ranges['F'+str(i)].value
+            #确定前置条件
+            if sheet_ranges['E'+str(i)].value:
+                preconditions = sheet_ranges['E'+str(i)].value
+                if ismerged(sheet_ranges['E'+str(i)]):
+                    preconditions0 = sheet_ranges['E'+str(i)].value
+            elif ismerged(sheet_ranges['E'+str(i)]):
+                preconditions=preconditions0
+            else:
+                preconditions = sheet_ranges['E'+str(i)].value
+                preconditions0 = ""
+            #步骤和期望结果
+            steps = sheet_ranges['F'+str(i)].value
+            expect = sheet_ranges['G'+str(i)].value
+            # print(sheet_ranges['B'+str(i)].value)
+            # print(suitename0)
+            # print("casename:"+str(casename))
+            # print("preconditions:"+str(preconditions))
+            # print("steps:"+str(steps))
+            # print("expect:"+str(expect))
             
-        #确定用例名
-        if sheet_ranges['D'+str(i)].value:
-            casename = sheet_ranges['D'+str(i)].value
-            casename = casename.replace("&","_").replace("\"","“")
-            casename0=casename
-            j = 2
-        elif casename0:
-            casename = "%s(%i)"%(casename0,j)
-            j+=1
-        elif sheet_ranges['E'+str(i)].value:
-            casename = sheet_ranges['E'+str(i)].value
-        else:
-            casename = sheet_ranges['F'+str(i)].value
-        #确定前置条件
-        if sheet_ranges['E'+str(i)].value:
-            preconditions = sheet_ranges['E'+str(i)].value
-            if ismerged(sheet_ranges['E'+str(i)]):
-                preconditions0 = sheet_ranges['E'+str(i)].value
-        elif ismerged(sheet_ranges['E'+str(i)]):
-            preconditions=preconditions0
-        else:
-            preconditions = sheet_ranges['E'+str(i)].value
-            preconditions0 = ""
-        #步骤和期望结果
-        steps = sheet_ranges['F'+str(i)].value
-        expect = sheet_ranges['G'+str(i)].value
-        # print(sheet_ranges['B'+str(i)].value)
-        # print(suitename0)
-        # print("casename:"+str(casename))
-        # print("preconditions:"+str(preconditions))
-        # print("steps:"+str(steps))
-        # print("expect:"+str(expect))
-        
-        case = TESTCASE % dict(
-                        casename=casename,
-                        preconditions=preconditions,
-                        steps=steps,
-                        expect=expect,
-        )
-        
-        # print(case)
-        cases.append(case)
-        i += 1
-    print(suitename0+":"+str(len(cases)))
-    suite = SUITE % dict(suitename = suitename0,testcases = "".join(cases))
-    cases = []
-    suites.append(suite)
-        
-    # print("".join(suites))
-        
-    final = SUITE % dict(suitename = sheetname.replace("&","_").replace("\"","“"),testcases = "".join(suites))
-        
-    f = open(result_dir+sheetname+".xml","wb")
-    f.write(HEADER.encode("utf8"))
-    f.write(final.encode("utf8"))
-    f.close
+            case = TESTCASE % dict(
+                            casename=casename,
+                            preconditions=preconditions,
+                            steps=steps,
+                            expect=expect,
+            )
+            
+            # print(case)
+            cases.append(case)
+            i += 1
+        print(suitename0+":"+str(len(cases)))
+        suite = SUITE % dict(suitename = suitename0,testcases = "".join(cases))
+        cases = []
+        suites.append(suite)
+            
+        # print("".join(suites))
+            
+        final = SUITE % dict(suitename = sheetname.replace("&","_").replace("\"","“"),testcases = "".join(suites))
+            
+        f = open(result_dir+sheetname+".xml","wb")
+        f.write(HEADER.encode("utf8"))
+        f.write(final.encode("utf8"))
+        f.close
 
 
 
